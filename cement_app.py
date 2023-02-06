5620e459bd14705cf95d4272cc791e0a760abb13
srihari50 <116938964+srihari50@users.noreply.github.com>
2023-02-06 13:41:09 +0530

    Update cement_app.py

--- cement_app.py
+++ cement_app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-%matplotlib inline
 import seaborn as sns
 from prophet import Prophet
 from prophet.plot import plot_plotly, plot_components_plotly
