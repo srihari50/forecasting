be91db6f0dde46b6445df6c68be541db902e5932
srihari50 <116938964+srihari50@users.noreply.github.com>
2023-02-07 17:37:47 +0530

    Update forecast.pickle.py

--- forecast.pickle.py
+++ forecast.pickle.py
@@ -34,7 +34,7 @@ def get_data(start, end):
 	start_row = 0
 	end_row = 0
 
-	for i in range(0, len(uploaded_file)):
+	for i in range(0, len(uploaded_file['Date'])):
 		if start <=	pd.to_datetime(uploaded_file['Date'][i]):
 			start_row = i
 			break
