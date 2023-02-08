138dc7072636d9771ad7a1a397151eca187f526f
srihari50 <116938964+srihari50@users.noreply.github.com>
2023-02-08 17:08:17 +0530

    Update forecasting.py

--- forecasting.py
+++ forecasting.py
@@ -11,7 +11,7 @@ st.title('Time Series Forecasting Using Streamlit')
 
 uploaded_files = st.file_uploader(" ", accept_multiple_files=True, type = ['xlxs'])
 if uploaded_files is not None:
-    for uploaded_file in uploaded_files:
+    for uploaded_files in uploaded_files:
          data = pd.read_excel(uploaded_file)
          future_data = pd.read_excel(uploaded_file)
     st.write(uploaded_file)
