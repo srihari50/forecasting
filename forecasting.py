f2e8fd03e022b11de38acfc6f65c70aabc5c182a
srihari50 <116938964+srihari50@users.noreply.github.com>
2023-02-08 17:18:27 +0530

    Update forecasting.py

--- forecasting.py
+++ forecasting.py
@@ -1,42 +1,21 @@
-import streamlit as st
+
+import streamlit as st 
 import pandas as pd
 import numpy as np
-from prophet import Prophet
-from prophet.diagnostics import performance_metrics
-from prophet.diagnostics import cross_validation
-from prophet.plot import plot_cross_validation_metric
-import base64  
+from fbprophet import Prophet
+from fbprophet.diagnostics import performance_metrics
+from fbprophet.diagnostics import cross_validation
+from fbprophet.plot import plot_cross_validation_metric
 
-st.title('Time Series Forecasting Using Streamlit')
+st.title('Cement Sales Forecasting')
 
-uploaded_files = st.file_uploader(" ", accept_multiple_files=True, type = ['xlxs'])
-if uploaded_files is not None:
-    for uploaded_file in uploaded_files:
-         data = pd.read_excel(uploaded_file)
-         future_data = pd.read_excel(uploaded_file)
-        
-st.write(data)
-st.write(cuture_data)
+data = st.file_uploader(' ',type='Xlsx', accept_multiple_files = True)
 
 if data is not None:
-    model = Prophet()
-    model.add_regressor('GDP_Construction_Rs_Crs')
-    model.add_regressor('GDP_Realestate_Rs_Crs')
-    model.add_regressor('Oveall_GDP_Growth%')
-    model.add_regressor('Water_Source')
-    model.add_regressor('Limestone')
-    model.add_regressor('Coal_Milliontonne')
-    model.add_regressor('Home_Interest_Rate')
-    model.add_regressor('Trasportation_Cost')
-    model.add_regressor('Order_Quantity_Milliontonnes')
-    model.add_regressor('Unit_Price')
-    model.fit(appdata)
-    future = model.make_future_dataframe(periods=12, freq = 'M')
-st.write(future)
-   
-forecast_data = model.predict(future)
-
-forecast_data[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(12)
+     appdata = pd.read_excel(data)
+     appdata['ds'] = pd.to_datetime(appdata['ds'],errors='coerce') 
+     st.write(data) 
+     
 
 
 
