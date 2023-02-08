5a58ab063caffd1ac9337cc6bbb3153ceab6ce4b
srihari50 <116938964+srihari50@users.noreply.github.com>
2023-02-08 17:44:52 +0530

    Update forecasting.py

--- forecasting.py
+++ forecasting.py
@@ -10,17 +10,15 @@ from prophet.plot import plot_cross_validation_metric
 st.title('Cement Sales Forecasting')
 
 files = st.file_uploader(' ',type='Xlsx')
-if files is not None:
-     data = pd.read_excel(files)
-     data = data.rename(columns={'Sales_Quantity_Milliontonnes': 'y', 'Date':'ds'})
-     data['ds'] = pd.to_datetime(appdata['ds'])
+for files in files:
+     if files is not None:
+          data = pd.read_excel(files)
+          data = data.rename(columns={'Sales_Quantity_Milliontonnes': 'y', 'Date':'ds'})
+          data['ds'] = pd.to_datetime(appdata['ds'])
+          newdata = pd.read_excel(files)
 st.write(files) 
+st.write(files)
 
-
-files2 = st.file_uploader(' ',type='Xlsx', accept_multiple_files = True)
-if files2 is not None:
-     newdata = pd.read_excel(files2)
-st.write(files2)
 
 
 
