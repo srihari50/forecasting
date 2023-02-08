b7190f20d0c7438845b99132df44f2a8533e9fc9
srihari50 <116938964+srihari50@users.noreply.github.com>
2023-02-08 18:28:15 +0530

    Update forecasting.py

--- forecasting.py
+++ forecasting.py
@@ -3,9 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from prophet import Prophet
-from prophet.diagnostics import performance_metrics
-from prophet.diagnostics import cross_validation
-from prophet.plot import plot_cross_validation_metric
+from prophet.plot import plot_plotly
 
 st.title('Cement Sales Forecasting')
 
