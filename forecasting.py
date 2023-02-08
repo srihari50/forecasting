9f161c6df3cb1cde934823ee1c9b0f951963cf6f
srihari50 <116938964+srihari50@users.noreply.github.com>
2023-02-08 17:32:22 +0530

    Update forecasting.py

--- forecasting.py
+++ forecasting.py
@@ -16,8 +16,8 @@ for data in data:
           newdata = pd.read_excel(data)
           appdata = appdata.rename(columns={'Sales_Quantity_Milliontonnes': 'y', 'Date':'ds'})
           appdata['ds'] = pd.to_datetime(appdata['ds'])
-     st.write(appdata) 
-     st.write(newdata)
+st.write(appdata) 
+st.write(newdata)
 
 
 
